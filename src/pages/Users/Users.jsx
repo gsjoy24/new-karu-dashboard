ff4dbd2227dc502a477f6ccb6b2e3985c392d72a
gjsoy24 <gour.joy24@gmail.com>
2024-09-16 10:15:22 +0600

    updated users page

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -19,7 +19,7 @@ import { useGetUserQuery } from '../../redux/features/userApi';
 import ChangeUserStatus from './ChangeUserStatus';
 
 const Users = () => {
-	const tableHeadings = ['#', 'Name', 'Email', 'Phone', 'Status', 'Created At', 'Actions'];
+	const tableHeadings = ['#', 'Name', 'Email', 'Status', 'Created At', 'Actions'];
 	const [page, setPage] = useState(1);
 	const [searchTerm, setSearchTerm] = useState('');
 	const { data, isFetching } = useGetUserQuery({
@@ -94,10 +94,9 @@ const Users = () => {
 							{data?.data?.map((user, index) => (
 								<TableRow key={user?._id}>
 									<TableCell align='center'>{index + 1}</TableCell>
-									<TableCell align='center'>{user?.full_name}</TableCell>
+									<TableCell align='center'>{user?.name}</TableCell>
 
 									<TableCell align='center'>{user?.email}</TableCell>
-									<TableCell align='center'>{user?.mobile_number || 'N/A'}</TableCell>
 									<TableCell align='center'>
 										<Chip
 											label={user?.status}
